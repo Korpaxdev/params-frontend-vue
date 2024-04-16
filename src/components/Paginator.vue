410c651268c85531b490c6a4f1c0acdee7a45a85
korpaxdev <korpaxdev@gmail.com>
2024-04-16 19:41:41 +0300

    change pagination in Paginator

--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -1,14 +1,35 @@
 <template>
   <nav aria-label="Page navigation example" class="mt-3">
     <ul class="pagination justify-content-center flex-wrap">
-      <li v-for="page in pageList" :key="page" class="page-item" :class="{ active: page === currentPage }">
+      <li class="page-item" :class="{ disabled: isStart }">
+        <button class="page-link" @click="changePage(DEFAULT_PAGE)">
+          <i class="bi-chevron-double-left"></i>
+        </button>
+      </li>
+      <li class="page-item" :class="{ disabled: isPrevDisabled }">
+        <button class="page-link" @click="downCurrentPageIndex">
+          <i class="bi-chevron-compact-left"></i>
+        </button>
+      </li>
+      <li v-for="page in visiblePages" :key="page" class="page-item" :class="{ active: page === currentPage }">
         <button class="page-link" @click="changePage(page)">{{ page }}</button>
+      </li>
+      <li class="page-item" :class="{ disabled: isNextDisabled }">
+        <button class="page-link" @click="upCurrentPageIndex">
+          <i class="bi-chevron-compact-right"></i>
+        </button>
+      </li>
+      <li class="page-item" :class="{ disabled: isEnded }">
+        <button class="page-link" @click="changePage(totalPages)">
+          <i class="bi-chevron-double-right"></i>
+        </button>
       </li>
     </ul>
   </nav>
 </template>
 <script lang="ts" setup>
 import { computed } from "vue";
+import { DEFAULT_PAGE } from "../utils/defaultConstants.ts";
 
 interface Props {
   currentPage: number;
@@ -19,4 +40,30 @@ interface Props {
 const props = defineProps<Props>();
 
 const pageList = computed(() => Array.from({ length: props.totalPages }).map((_, i) => i + 1));
+const pageListPaginator = computed(() => {
+  const pages = [];
+  for (let i = 0; i < pageList.value.length; i += 3) {
+    pages.push(pageList.value.slice(i, i + 3));
+  }
+  return pages;
+});
+const currentPaginatorIndex = computed(() => {
+  return pageListPaginator.value.findIndex((pages) => pages.some((page) => page === props.currentPage));
+});
+const visiblePages = computed(() => pageListPaginator.value[currentPaginatorIndex.value]);
+
+const upCurrentPageIndex = () => {
+  if (currentPaginatorIndex.value < pageListPaginator.value.length - 1) {
+    props.changePage(pageListPaginator.value[currentPaginatorIndex.value + 1][0]);
+  }
+};
+const downCurrentPageIndex = () => {
+  if (currentPaginatorIndex.value > 0) {
+    props.changePage(pageListPaginator.value[currentPaginatorIndex.value - 1][0]);
+  }
+};
+const isNextDisabled = computed(() => currentPaginatorIndex.value === pageListPaginator.value.length - 1);
+const isPrevDisabled = computed(() => currentPaginatorIndex.value === 0);
+const isEnded = computed(() => props.currentPage === props.totalPages);
+const isStart = computed(() => props.currentPage === DEFAULT_PAGE);
 </script>
